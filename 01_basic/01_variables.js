4b33e4592356d88fba868fc5972c894a16093d37
Vedansh Singh <vedanshsingh975@gmail.com>
2026-05-22 07:34:29 +0000

    conversion of datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+let accountName = "vedansh singh"
+const accountId = 199
+let state //assign undefined
+
+console.table([accountId,accountName,state])
+
+console.log(typeof null);  // object 
+console.log(typeof undefined);  //undefined
+
---
+++ 01_basic/02_conversionOperations.js
@@ -0,0 +1,11 @@
+let score = "33aa"
+console.log(typeof score);
+let conv = Number(score)
+console.log(conv); // Nan
+console.log(typeof conv) // number
+
+let LoggedIn = "vedansh"
+let ConvLoggedIn = Boolean(LoggedIn)
+console.log(ConvLoggedIn); // false and true for "vedansh"
+
+
